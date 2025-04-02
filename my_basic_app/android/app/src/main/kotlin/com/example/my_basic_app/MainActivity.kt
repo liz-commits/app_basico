712ee8d4c84665cffd4d88e9c9c35efcd1bd5b69
liz-commits <elizeuen45@gmail.com>
2025-04-02 19:00:51 +0000

    Adicionado workflow para compilar e assinar APK

---
+++ my_basic_app/android/app/src/main/kotlin/com/example/my_basic_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.my_basic_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ my_basic_app/lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Hello GitHub Actions',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Hello GitHub Actions')),
+      body: const Center(child: Text('Hello, GitHub Actions!', style: TextStyle(fontSize: 24))),
+    );
+  }
+}
